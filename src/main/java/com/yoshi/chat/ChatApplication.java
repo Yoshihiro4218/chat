d371cfe40cd606bafff5d458849fb8d1327f3ed2
Yoshihiro4218 <nana.mizuki77@icloud.com>
2020-03-14 22:16:59 +0900

    first commit

---
+++ src/main/java/com/yoshi/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.yoshi.chat;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+
+}
