d2444ba523ec348c329f043ea576401296dacc6e
Yoshihiro4218 <nana.mizuki77@icloud.com>
2020-03-15 10:42:20 +0900

    見た目ちょっと修正

--- src/main/java/com/yoshi/chat/app/controller/ChatFormController.java
+++ src/main/java/com/yoshi/chat/app/controller/ChatFormController.java
@@ -4,7 +4,6 @@ import com.yoshi.chat.app.properties.*;
 import com.yoshi.chat.domain.entity.*;
 import com.yoshi.chat.domain.service.*;
 import lombok.*;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @Controller
+@RequestMapping("/form")
 @AllArgsConstructor
 public class ChatFormController {
     private final ChatLogService chatLogService;
