52832df878f787e7bf3febc58d90858b4f752035
yoshihiro <y.kadono4218@gmail.com>
2020-09-08 22:25:28 +0900

    システムインフォを赤文字にした ほか

--- src/main/java/com/yoshi/chat/app/controller/WebSocketEventListener.java
+++ src/main/java/com/yoshi/chat/app/controller/WebSocketEventListener.java
@@ -10,15 +10,10 @@ import org.springframework.web.socket.messaging.*;
 @Slf4j
 public class WebSocketEventListener {
 
-    @EventListener
-    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
-        GenericMessage message = (GenericMessage) event.getMessage();
-        String simpDestination = (String) message.getHeaders().get("simpDestination");
-
-        log.info("Event.getMessage()={}", message);
-
-//        if (simpDestination.startsWith("/topic/group/1")) {
-//            // do stuff
-//        }
-    }
+//    @EventListener
+//    public void handleSessionSubscribeEvent(SessionSubscribeEvent event) {
+//        GenericMessage message = (GenericMessage) event.getMessage();
+//        String simpDestination = (String) message.getHeaders().get("simpDestination");
+//        log.info("Event.getMessage()={}", message);
+//    }
 }
